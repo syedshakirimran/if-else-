6e7252c089a87ecef099fbfb05974aa97fecd219
syed shakir imran <syedabbas0331@gmail.com>
2017-03-11 18:15:51 -0800

    if-else nested code

---
+++ app.js
@@ -0,0 +1,22 @@
+var a = +prompt ("ENTER YOU PERCENTAGE");
+if(a<=100 && a>=85){
+ alert("YOUR GRADE IS A");
+}
+else if (a<85 && a>=80){
+    alert("YOUR GRADE IS B+");
+}
+else if (a<80 && a>=72){
+    alert("YOUR GRADE IS B");
+}
+else if (a<72 && a>=65){
+    alert("YOUR GRADE IS C+");
+}
+else if (a<65 && a>=60){
+    alert("YOUR GRADE IS C");
+}
+else if (a<60 && a>=50){
+    alert("YOUR GRADE IS D");
+}
+else if (a<50 && a>=0) {
+    alert("YOUR ARE FAILED"); 
+}
